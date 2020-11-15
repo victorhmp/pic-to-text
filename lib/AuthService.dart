9670f5c0a1937d6c31913979c55f51e4839fb526
victorhmp <victor2142@gmail.com>
2020-11-15 20:46:26 -0300

    Setup firestone connection and users database

--- lib/AuthService.dart
+++ lib/AuthService.dart
@@ -3,9 +3,11 @@ import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthService with ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   final GoogleSignIn googleSignIn = GoogleSignIn();
 
   Future<User> getUser() {
@@ -37,6 +39,15 @@ class AuthService with ChangeNotifier {
           await _auth.signInWithCredential(credential);
 
       final User user = authResult.user;
+
+      var userDocument =
+          await _firestore.collection('users').doc(user.uid).get();
+
+      if (!userDocument.exists) {
+        _firestore.collection('users').doc(user.uid).set(
+          {'history': []},
+        );
+      }
 
       notifyListeners();
 
